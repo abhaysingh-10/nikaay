65b782ca07f0a8b4b1eddcf0eeafab87caf6af45
Abhay Singh <aiwithabhay7474452@gmail.com>
2026-07-14 15:13:03 +0530

    feat: configure firebase options and initialize Firebase in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart'; //temp (need to remove afterwards)
-
 import 'app/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
   //temp (need to remove afterwards)
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences.getInstance().then((prefs) => prefs.clear());
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const ProviderScope(
       child: NikaayApp(),
