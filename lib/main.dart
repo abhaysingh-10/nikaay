dcc4fa78fc9beb48e15b3c944d07cf7a3bea91dd
Abhay Singh <aiwithabhay7474452@gmail.com>
2026-07-15 21:54:26 +0530

    chore: add temporary testing mode toggle to alternate onboarding states on restarts

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart'; //temp (need to remove afterwards)
+import 'package:shared_preferences/shared_preferences.dart';
 import 'app/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences.getInstance().then((prefs) => prefs.clear()); //temp (need to remove afterwards)
+
+  //  ------TESTING MODE TOGGLE ------ (delete it later )
+  final prefs = await SharedPreferences.getInstance();
+  final runCount = prefs.getInt('temp_testing_run_count') ?? 0;
+  final nextRunCount = runCount + 1;
+  await prefs.setInt('temp_testing_run_count', nextRunCount);
+
+  if (nextRunCount % 2 == 1) {
+    
+    await prefs.setBool('onboarding_completed', false);
+  } else {
+    await prefs.setBool('onboarding_completed', true);
+  }
+  // -----------
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
