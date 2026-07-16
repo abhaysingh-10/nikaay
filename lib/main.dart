df4adeb2f4da779d0ef37beeadf8ce827781ff45
Abhay Singh <aiwithabhay7474452@gmail.com>
2026-07-16 14:54:40 +0530

    chore: add Firebase sign-out to main testing toggle

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart'; //delete it later
 import 'app/app.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  //  ------TESTING MODE TOGGLE ------ (delete it later )
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // ------TESTING MODE TOGGLE ------ (delete it later)
   final prefs = await SharedPreferences.getInstance();
   final runCount = prefs.getInt('temp_testing_run_count') ?? 0;
   final nextRunCount = runCount + 1;
   await prefs.setInt('temp_testing_run_count', nextRunCount);
   if (nextRunCount % 2 == 1) {
     await prefs.setBool('onboarding_completed', false);
+    await FirebaseAuth.instance.signOut();
   } else {
     await prefs.setBool('onboarding_completed', true);
   }
   // -----------
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(
     const ProviderScope(
       child: NikaayApp(),
