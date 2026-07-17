091e864d0844602c933b893210ccb77f4b3df427
Abhay Singh <aiwithabhay7474452@gmail.com>
2026-07-17 12:25:47 +0530

    feat(navigation): implement bottom navigation  and route   names

--- lib/app/routes/route_names.dart
+++ lib/app/routes/route_names.dart
@@ -1,7 +1,11 @@
 class RouteNames {
-      static const String splash = '/';
-      static const String onboarding = '/onboarding';
-      static const String login = '/login';
-      static const String signup = '/signup';
-      static const String home = '/home';
-    }
+  static const String splash = '/';
+  static const String onboarding = '/onboarding';
+  static const String login = '/login';
+  static const String signup = '/signup';
+  static const String home = '/home';
+  static const String chat = '/chat';
+  static const String history = '/history';
+  static const String profile = '/profile';
+  static const String settings = 'settings';
+}
